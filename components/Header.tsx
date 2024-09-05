6bd61626fa20e7e54ac08c137272a50592bcdfdf
pablo <pablolimadeoliveira77@gmail.com>
2024-09-05 18:05:20 -0300

    Site Página Prinipal

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
 
     function handleOpenMenu() {
         console.log("Clicou no botão de menu");
-        
+
         setMenuOpen(!menuOpen);
     }
     return (
@@ -15,6 +15,7 @@ export default function Header() {
             {/* Primeira Coluna (Logotipo) com fundo mais claro e 15% de largura */}
             <div className="bg-nf1-fundohead flex items-center p-4 w-full md:w-1/6 min-w-44">
                 <img src="img/nf-branco.png" alt="Logotipo" className="h-20 mx-auto" />
+
                 <button className="md:hidden flex" onClick={handleOpenMenu}>
                     <FaBars className="text-nf1-branco text-2xl" />
                 </button>
@@ -42,7 +43,7 @@ export default function Header() {
             {/* Menu de Navegação para Dispositivos Móveis */}
             {menuOpen && (
                 <div className="bg-nf1-dark block md:flex text text-nf1-branco flex-wrap justify-between items-center p-4 w-full md:w-5/6">
-                    <nav className="flex-grow flex flex-col justify-center items-center gap-5 lg:text-lg md:text-sm">
+                    <nav className="flex-grow flex flex-col justify-center items-center gap-3 lg:text-lg md:text-sm">
                         <a href="#" className="hover:text-nf1-fundohead transition">IGREJA</a>
                         <a href="#" className="hover:text-nf1-fundohead transition">CONECTE-SE</a>
                         <a href="#" className="hover:text-nf1-fundohead transition">EVENTOS</a>
@@ -50,7 +51,7 @@ export default function Header() {
                         <a href="#" className="hover:text-nf1-fundohead transition">DOAÇÕES</a>
                     </nav>
                     <div className="flex items-center justify-center">
-                        <button className="bg-nf1-branco text-nf1-fundo font-semibold py-2 px-6 lg:text-lg md:text-sm rounded-full hover:bg-nf1-fundo1 transition">
+                        <button className="bg-nf1-branco text-nf1-fundo font-semibold py-2 px-6 lg:text-lg md:text-sm rounded-full hover:bg-nf1-fundo1 transition mt-3">
                             LOGIN
                         </button>
                     </div>
