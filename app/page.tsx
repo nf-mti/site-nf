893502137b2885e6e6420273a755757bcd9d34ba
Vitor <vitor.emerick@audora.com.br>
2024-09-05 17:56:43 -0300

    missão com animação

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import SlideCard from "@/components/SlideCard";
 import Pastores from "@/components/Pastores";
 import Footer from "@/components/footer";
 import Header from "@components/Header";
+import Missao from "@/components/Missao";
+
+
 
 
 export default function Home() {
@@ -100,11 +103,15 @@ export default function Home() {
         </h1>
         <SlideCard dados={novidades} />
       </section>
+
+      <section id="Missao" className=" bg-nf1-fundo text-nf1-branco">
+        <Missao />
+      </section>
       <section id="Pastores" className="w-full px-4 py-2  bg-nf1-fundo  text-nf1-branco mt-1 mb-1 ">
         <Pastores />
       </section>
       <section className="w-full px-4 py-2  bg-nf1-fundo text-nf1-branco ">
-        <Footer/>
+        <Footer />
       </section>
     </main>
   );
